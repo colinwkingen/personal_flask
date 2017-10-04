651926bffc7966e91a8dcc5eed12df4cd12ea0b6
Colin Kingen <colinwkingen@gmail.com>
2017-10-03 23:27:35 -0700

    change some imports

--- home.py
+++ home.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
- 
+
 if __name__ == "__main__":
+    
     app.run("0.0.0.0", 80, debug=True, use_reloader=False)
