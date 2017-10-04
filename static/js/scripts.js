38f8c892cad05fcc7b38f698f4ffccb5e7cd4f32
Colin Kingen <colinwkingen@gmail.com>
2017-10-04 00:16:25 -0700

    generate background color js added

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,16 @@
 
 
 $(document).ready(function() {
-
+	 function generateColor() {
+		 console.log("Generating...");
+		 var randomColor = "#";
+		 var hexArr = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+		 for (i=0;i<6;i++) {
+			 var index = Math.floor(Math.random()*15);
+			 console.log("Index... : "+index);
+			 randomColor+= hexArr[index];
+		 }
+		 return randomColor;
+	 }
+	 $('.secondary').css('background-color',generateColor()); 
 });
